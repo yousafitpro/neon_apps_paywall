c6a2649fe2afe3f8e4e19302f81ff47c68e60fd2
Muhammad Yousaf <yousaf.itpro@gmail.com>
2024-01-08 00:51:11 +0500

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,11 +46,7 @@
                                 </li>
                             @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+
                         @else
                             <li class="nav-item dropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
