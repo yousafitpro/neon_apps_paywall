c6b512dc0999f2325f2d4fee89c544311a4b3b82
Muhammad Yousaf <yousaf.itpro@gmail.com>
2024-01-08 00:37:50 +0500

    ok

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 //adsdasd
- //Auth::routes();
+ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
