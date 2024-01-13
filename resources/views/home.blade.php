866eca02fc5b440193f8aca3dafd58b05e32fd82
Muhammad Yousaf <yousaf.itpro@gmail.com>
2024-01-13 13:29:20 +0500

    ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10 offset-md-1">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" style="overflow: auto">
 
                     <table id="yourDataTable">
                         <thead>
