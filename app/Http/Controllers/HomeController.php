85a17bbb5bc5eda77aee09de84f4e2428dd28840
Muhammad Yousaf <yousaf.itpro@gmail.com>
2024-09-16 14:05:43 -0700

    asdasd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -179,8 +179,8 @@ class HomeController extends Controller
                     'is_directly_subscribed'=>self::count_event_ratio_and_number($bundle_id,'is_directly_subscribed',$startDate->toDateTimeString(),$endDate->toDateTimeString()),
                     'to_be_paid'=>self::count_event_ratio_and_number($bundle_id,'is_trial_converted',$startDate->toDateTimeString(),$endDate->toDateTimeString(),'!='),
                     'total_events' => $event->total_events,
-                    'start_date' => $startDate->format('y'),
-                    'end_date' => $endDate->format('y'),
+                    'start_date' => $startDate->format('Y'),
+                    'end_date' => $endDate->format('Y'),
                 ];
             });
         }
